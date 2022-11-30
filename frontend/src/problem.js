80b816ba5344fde8b913bae1bcbdfde111317084
rjsdn1120 <rjsdn1120@gmail.com>
2022-12-01 04:33:20 +0900

    UI 수정

--- frontend/src/problem.js
+++ frontend/src/problem.js
@@ -2,7 +2,7 @@ import './problem.css'
 function Problem(){
     return(
         <div className="section_problem">
-            <div className='problem_head'>문제설명</div>
+            <div className='problem_head'>문제 설명</div>
         </div>
     )
 }
